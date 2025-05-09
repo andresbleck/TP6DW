44c2a23497b4dd4c2277283d7b8cbb5e5d89b6e1
andresbleck <andres.bleckwedel2@gmail.com>
2025-05-09 18:16:09 -0300

    Update data.js

--- App1/data.js
+++ App1/data.js
@@ -1,1 +1,39 @@
+let productos = [];
 
+function obtenerProductos() {
+  return productos;
+}
+
+function agregarProducto(producto) {
+  productos.push(producto);
+  return producto;
+}
+
+function eliminarProducto(nombre) {
+  const index = productos.findIndex(p => p.nombre === nombre);
+  if (index !== -1) {
+    const eliminado = productos.splice(index, 1);
+    return eliminado[0];
+  }
+  return null;
+}
+
+function modificarProducto(nombre, nuevosDatos) {
+    const producto = productos.find(p => p.nombre === nombre);
+    if (!producto) return null;
+  
+    if (nuevosDatos.precio !== undefined) {
+      producto.precio = nuevosDatos.precio;
+    }
+    if (nuevosDatos.descripcion !== undefined) {
+      producto.descripcion = nuevosDatos.descripcion;
+    }
+    return producto;
+  }
+  
+  module.exports = {
+    obtenerProductos,
+    agregarProducto,
+    eliminarProducto,
+    modificarProducto
+  };
